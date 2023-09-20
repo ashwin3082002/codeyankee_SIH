51c2668184d93a4b515e4ec7dae7241ed0b4890b
aviiciii <laavesh1@gmail.com>
2023-09-20 19:32:01 +0530

    changes

--- nasha/urls.py
+++ nasha/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path("__reload__/", include("django_browser_reload.urls")),
     path('testing', views.testing, name='testing')
+    
 ]
