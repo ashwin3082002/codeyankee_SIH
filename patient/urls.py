0be7a5f03b89548eebe8e748a05677ef143257f6
Sasukexo <aswinash1002@gmail.com>
2023-09-20 20:43:55 +0530

    added view

--- patient/urls.py
+++ patient/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 
 urlpatterns = [
-     path("login/", views.login_view, name="login"),
-    path('medicalcen',views.medicalcen),
-    path('admin',views.admin),
+    path("login/", views.login_view, name="login"),
+    # path('medicalcen',views.medicalcen),
     path("logout/", views.logout_view, name="logout"),
 ]
--- patient/views.py
+++ patient/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.db import IntegrityError
 # Create your views here.
 
 def login_view(request):
@@ -25,10 +26,39 @@ def login_view(request):
             if user.is_superuser:
                 return redirect('admin')
             else:
-                return redirect('patient')
+                return redirect('login')
         else:
             messages.error(request, 'Invalid username and/or password.')
             return redirect('login')
     else:
         return render(request, "login/login.html")
     
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+
+
+def register(request):
+    if request.method == "POST":
+            username = request.POST["team_name"]
+            password=request.POST["password"]
+            try:
+                user = User.objects.create_user(username, password)
+                user.save()           
+
+
+            except:
+                messages.error(request, '"Username already taken"')
+                return redirect('register')
+            messages.success(request, 'Patient registered successfully!')
+            return redirect('land')
+    else:
+            return render(request, "login/register.html")
+    
+
+
+    def land(request):
+        return render(request, 'patient/index.html')
