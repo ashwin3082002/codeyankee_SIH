58892fddca33f4c89daa2b5e1aa1c27b1d5c295e
aviiciii <laavesh1@gmail.com>
2023-09-21 11:08:08 +0530

    login fix

--- nasha/views.py
+++ nasha/views.py
@@ -15,9 +15,10 @@ def login_view(request):
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            print("hello")
+            
             search_details = user_roles.objects.filter(username=username)
-            if search_details == "admin":
+            if search_details[0].role == "admin":
+                print("hello")
                 login(request, user)
                 return redirect('hospital_dashboard')
             # login(request, user)
