da095c3dc3ae7f028aa02ac979bf736d7289db0d
Sasukexo <aswinash1002@gmail.com>
2023-09-22 07:56:54 +0530

    removed package

--- patient/views.py
+++ patient/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-import gradio,openai
+
 # Create your views here.
 
 def login_view(request):
